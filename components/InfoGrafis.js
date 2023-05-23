fbc7d8d0c8bd22065437196c05d436237d10c09f
pudyasta <pudyastasatria@gmail.com>
2023-05-23 18:26:54 +0700

    Add fkkmk on compatible device

--- components/InfoGrafis.js
+++ components/InfoGrafis.js
@@ -1,10 +1,8 @@
 import React, { Suspense, useState } from "react";
 import { Interactive, XR, XRButton } from "@react-three/xr";
-import { Environment, OrbitControls, Text } from "@react-three/drei";
+import { OrbitControls, Text } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { ResizeObserver } from "@juggle/resize-observer";
-import { useNavigate } from "react-router-dom";
-import ModelThree from "./ModelThree";
 import { useRouter } from "next/router";
 
 const Box = ({ color, size, scale, children, ...rest }) => {
@@ -17,35 +15,34 @@ const Box = ({ color, size, scale, children, ...rest }) => {
   );
 };
 
-const Button = (props) => {
-  const [hover, setHover] = useState(false);
-  const [color, setColor] = useState("pink");
-
-  const onSelect = () => {
-    setColor((Math.random() * 0xffffff) | 0);
-  };
-
+const BoxGeometric = (props) => {
   return (
-    <Interactive
-      onHover={() => setHover(true)}
-      onBlur={() => setHover(false)}
-      onSelect={onSelect}
-    >
-      <Box
-        color={color}
-        scale={hover ? [0.6, 0.6, 0.6] : [0.5, 0.5, 0.5]}
-        size={[0.45, 0.5, 0.1]}
-        {...props}
-      >
+    <Interactive>
+      <Box color="fff" {...props}>
         <Suspense fallback={null}>
           <Text
-            position={[0, 0, 0.06]}
-            fontSize={0.05}
+            position={[0, 0.25, 0.1]}
+            fontSize={0.06}
+            lineHeight={1.5}
             color="#000"
             anchorX="center"
             anchorY="middle"
+            overflowWrap="true"
+            maxWidth={0.7}
           >
-            Pionir Gadjah Mada
+            {props.header}
+          </Text>
+          <Text
+            position={[0, 0, 0.06]}
+            fontSize={0.03}
+            lineHeight={1.5}
+            color="#000"
+            anchorX="center"
+            anchorY="middle"
+            overflowWrap="true"
+            maxWidth={0.7}
+          >
+            {props.children}
           </Text>
         </Suspense>
       </Box>
@@ -77,8 +74,57 @@ const InfoGrafis = () => {
             <>
               <ambientLight />
               <pointLight position={[10, 10, 10]} />
-              <Button position={[0, 0.1, -0.8]} />
-              <ModelThree position={[5, 0, -5]} scale={[0.008, 0.008, 0.008]} />
+              <Suspense fallback={null}>
+                <Text
+                  position={[0.15, 0.89, -1.3]}
+                  fontSize={0.3}
+                  color="#000"
+                  anchorX="center"
+                  anchorY="middle"
+                  overflowWrap="true"
+                >
+                  FKKMK
+                </Text>
+              </Suspense>
+              <BoxGeometric
+                header="Deskripsi"
+                position={[0.1, 0.1, -1.2]}
+                size={[0.9, 0.7, 0.1]}
+              >
+                FK-KMK memiliki tujuan untuk menjadi Fakultas berkelas dunia
+                yang inovatif dan unggul,serta senantiasa mengabdi kepada bangsa
+                dan kemanusiaan. FK-KMK berdiri pada 5 Maret 1946. Sebagai
+                fakultas kedokteran tertua di Indonesia, FK-KMK awalnya
+                merupakan sekolah kedokteran yang didirikan pada zaman
+                penjajahan belanda, yakni School tot Opleiding van Indische
+                Artsen (STOVIA), sampai akhirnya bergabung dengan Universitas
+                Gadjah Mada pada 16 Desember 1949.
+              </BoxGeometric>
+              <BoxGeometric
+                position={[-1, 0.1, -0.6]}
+                rotation-y={Math.PI * 0.25}
+                size={[0.9, 0.7, 0.1]}
+                header="Fasilitas"
+              >
+                - Perpustakaan FK-KMK - Gedung Perkuliahan (Grha Wiyata, Ruang
+                Kuliah dan Auditorium, Gedung Ismangoen, Gedung Dept. Gizi dan
+                Kesehatan, Gedung Radiopoetro, dll.) - Lapangan Basket - Kantin
+                (Kantin Medika, Kantin IKM, Kantin Putri) - Masjid Ibnu Sina -
+                Student Centre
+              </BoxGeometric>
+              <BoxGeometric
+                header="PPSMB Morfogenesis"
+                position={[1.01, 0.1, -0.6]}
+                rotation-y={49}
+                size={[0.9, 0.7, 0.1]}
+              >
+                PPSMB Morfogenesis merupakan kegiatan orientasi dan pengenalan
+                kampus FK-KMK bagi ‘Sardjito Muda’ yang bertujuan untuk
+                mewujudkan lingkungan pembelajaran yang nyaman, saling
+                menghormati, dan saling menghargai.
+              </BoxGeometric>
+
+              {/* <ModelThree position={[5, 0, -5]} scale={[0.008, 0.008, 0.008]} /> */}
             </>
           ) : (
             <>
@@ -86,6 +132,7 @@ const InfoGrafis = () => {
               {/* <Environment files="/ok2.hdr" background /> */}
             </>
           )}
+          <axesHelper />
         </XR>
       </Canvas>
     </div>
